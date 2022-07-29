4615c19e88764af59667353e79589f348bedb36c
tiago graffunder <tiagopinzgraffunder@gmail.com>
2022-07-29 14:42:01 -0300

    estudando...

---
+++ PHP-l/tipos.php
@@ -0,0 +1,10 @@
+<?php
+//não é necessário dizer o tipo do valor na variável
+//TIPOS PRIMITIVOS:
+$idade = 21; //inteiro
+$salario = 1000.301; //double (mais precisão) or float
+$texto = "ola mundo"; //string
+$verdadeiro = true; //boolean
+$falso = false; //boolean
+
+echo gettype($texto); //pega o tipo da variávle

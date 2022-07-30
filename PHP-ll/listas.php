f55af1903e74876d7f1ea4847ecc39e3b79f9543
tiago graffunder <tiagopinzgraffunder@gmail.com>
2022-07-30 15:37:49 -0300

    estudando...

--- PHP-ll/listas.php
+++ PHP-ll/listas.php
@@ -3,3 +3,9 @@
 $idadeList = [21,23,19,25,30,41,10]; //índices
 $primeiraIdade = $idadeList[0];
 echo $primeiraIdade;
+
+//adcionando
+$idadeList[] = 20;
+foreach($idadeList as $idade){
+    echo $idade.PHP_EOL;
+}

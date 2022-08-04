c3a322ce247c2ee0c1b826cee2a2c47c438f6716
tiago graffunder <tiagopinzgraffunder@gmail.com>
2022-08-04 16:36:28 -0300

    estudando...

--- php-04/src/Conta.php
+++ php-04/src/Conta.php
@@ -4,5 +4,33 @@ class Conta //classe -> formas | cria um novo tipo
     //definir dados da conta
     public string $cpfTitular;//atributos
     public string $nomeTitular;
-    public float  $saldo;
+    public float  $saldo = 0;
+
+    public function sacar(float $valorASacar) //método
+    {
+        if ($valorASacar > $this->saldo){ //dessa classe se refere ao objeto que "chamou" o método
+            echo "saldo indisponível";
+        }else{
+            $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar($valorADepositar): void 
+    {
+        if($valorADepositar < 0){
+            echo "valor precisa ser positvo";
+        }else{
+            $this->saldo += $valorADepositar;
+        }
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void 
+    {
+        if($valorATransferir > $this->saldo){
+            echo "saldo indisponivel";
+        }else{
+            $this->sacar($valorATransferir);
+            $contaDestino->depositar($valorATransferir);
+        }
+    }
 } 
